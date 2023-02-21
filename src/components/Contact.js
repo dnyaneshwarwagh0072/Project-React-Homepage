cb0e46490bc3fb6ffa5794b42f91113721ddbfb6
Dnyaneshwar Yadav Wagh <85550894+nanawagh@users.noreply.github.com>
2023-02-21 14:27:23 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,10 @@ const Contact = _ => {
     return (
         <section>
             <h3>📞Contact</h3>
-            <a href="https://www.linkedin.com/in/dnyaneshwar-wagh-4166a1227/" target='_blank'>LinkedIn</a>
-            <a href="https://github.com/" target='_blank'>Github</a>
-            <a href="https://www.instagram.com/the_tiger.0072/" target='_blank'>Instagram</a>
-            <a href="https://www.facebook.com/profile.php?id=100029032247800" target='_blank'>Facebook</a>
+            <a href="https://www.linkedin.com/in/dnyaneshwar-wagh-4166a1227/" target='_blank' rel="noreferrer">LinkedIn</a>
+            <a href="https://github.com/" target='_blank' rel="noreferrer">Github</a>
+            <a href="https://www.instagram.com/the_tiger.0072/" target='_blank' rel="noreferrer">Instagram</a>
+            <a href="https://www.facebook.com/profile.php?id=100029032247800" target='_blank' rel="noreferrer">Facebook</a>
         </section>
     );
 }
